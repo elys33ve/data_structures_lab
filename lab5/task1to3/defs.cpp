8b5f23415b733d5313b6964456d5da98d4fc5f5d
ayusi35 <84962606+ayusi35@users.noreply.github.com>
2023-02-15 16:20:04 -0500

    Update defs.cpp

--- lab5/task1to3/defs.cpp
+++ lab5/task1to3/defs.cpp
@@ -28,3 +28,23 @@ void Shelf::remove_movie(string m){
 	}
 }
 
+// SETTER
+void Movie::set_credits(std::string c) {
+	credits = c;
+}
+
+// GETTER
+std::string Movie::get_credits() {
+	return credits;
+}
+
+// Play
+void Movie::Play() {
+	std::cout << "credits: " << credits << std::endl;
+}
+
+
+// default constructor
+Shelf::Shelf() {
+	num_movies = 0;
+}
