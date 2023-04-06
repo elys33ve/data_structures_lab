f1651ba0a4962d023e08cb1a9ba571dc6866d0f9
fiona <fiona@cincycircle.com>
2023-04-06 19:16:00 -0400

    balances better but infinite loop somewhere

--- lab9/Tree.cpp
+++ lab9/Tree.cpp
@@ -3,5 +3,32 @@
 
 
 
- 
+template<class T>
+void Tree<T>::insert(T item) {
 
+
+// first item
+// second item
+
+// balance thrid
+bal(find_parent(parent))
+
+}
+
+balance(Node<T> *node) {
+
+
+	if (height(node) != corr_height() && height(node) > 2) {
+			balance
+	}
+
+
+
+}
+
+find parent(node) {
+	if node = nullptr {rtn nullptr}
+	
+	if node== root {rtn root}
+
+}
